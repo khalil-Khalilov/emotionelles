dcdd9565313ca36f2bd18c32b2f8463fa485d13a
Khalil <you@example.com>
2021-01-15 09:21:28 +0100

    page actualites

--- src/Controller/ActualiteController.php
+++ src/Controller/ActualiteController.php
@@ -2,19 +2,70 @@
 
 namespace App\Controller;
 
+use App\Entity\Actualite;
+use App\Form\ActualiteType;
+use App\Repository\ActualiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ActualiteController extends AbstractController
 {
     /**
-     * @Route("/actualite", name="actualite")
-     */
-    public function index(): Response
+     * @Route("/actualites", name="actualites")
+    */
+    public function actualites(ActualiteRepository $actualiteRepository): Response
     {
-        return $this->render('actualite/index.html.twig', [
-            'controller_name' => 'ActualiteController',
+        $actualites = $actualiteRepository->findAll();
+
+        return $this->render('actualite/actualites.html.twig', [
+            "actualites"=>$actualites,
+        ]);
+    }
+
+    /**
+     * @Route("/actualite/{id}", name="actualite", requirements={"id":"\d+"})
+    */
+    public function actualite(ActualiteRepository $actualiteRepository, $id): Response
+    {
+
+        $actualite = $actualiteRepository->find($id);
+
+        return $this->render('actualite/actualite.html.twig', [
+            "actualite"=>$actualite,
+        ]);
+    }
+
+    /**
+     * @Route("/actualite/back", name="actualiteBack")
+    */
+    public function actualiteBack(Request $request): Response
+    {
+
+        $actualite = new Actualite();
+
+        $form = $this->createForm(ActualiteType::class, $actualite);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            if($form->isValid()){
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($actualite);
+                $em->flush();
+                
+                $this->addFlash('success', "L'article a bien été enregistré.");
+                return $this->redirectToRoute('actualites');
+            }else {
+                $this->addFlash('danger', "Le formulaire comporte des erreurs.");
+            }
+        }
+
+        return $this->render('actualite/actualiteBack.html.twig', [
+            "form" => $form->createView(),
+            "actualite" => $actualite,
         ]);
     }
 }
---
+++ src/Entity/Actualite.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ActualiteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ActualiteRepository::class)
+ */
+class Actualite
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lieu;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $facebookEvent;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getLieu(): ?string
+    {
+        return $this->lieu;
+    }
+
+    public function setLieu(string $lieu): self
+    {
+        $this->lieu = $lieu;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getFacebookEvent(): ?string
+    {
+        return $this->facebookEvent;
+    }
+
+    public function setFacebookEvent(?string $facebookEvent): self
+    {
+        $this->facebookEvent = $facebookEvent;
+
+        return $this;
+    }
+}
